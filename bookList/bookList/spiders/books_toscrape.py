ed45c01aaeab005d8d662313faf750e25f9a16b8
Stefane Brito <stefane_brito@hotmail.com>
2023-03-30 15:54:16 -0300

    commit inicial

--- bookList/bookList/spiders/books_toscrape.py
+++ bookList/bookList/spiders/books_toscrape.py
@@ -3,8 +3,13 @@ import scrapy
 
 class BooksToscrapeSpider(scrapy.Spider):
     name = "books.toscrape"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["http://books.toscrape.com/"]
+    #allowed_domains = ["books.toscrape.com"]
+    start_urls = ["http://books.toscrape.com/index.html"]
 
     def parse(self, response):
+        for livros in response.css('.col-lg-3'):
+            yield{
+                'title' : livros.css('h3 a::text'),
+                'preco' : livros.css('.price_color ::text')
+            }
         pass
